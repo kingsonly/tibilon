e79fbdfc82947b3335af1fdc77ec8b9a502613e0
kingsley achumie <kingsonly13c@gmail.com>
2023-07-26 20:34:19 +0100

    wip

--- frontend/src/components/FileTypeComponent.jsx
+++ frontend/src/components/FileTypeComponent.jsx
@@ -3,11 +3,8 @@ import pdfIcon from "../assests/pdf-icon-file.svg";
 import docsIcon from "../assests/docs-icon.svg";
 import { AiFillDelete, AiFillEdit, AiFillEye } from "react-icons/ai";
 import DialogModal from "./DialogModal";
-import AppModal from "./AppModal";
-import DocumentsContent from "./DocumentsContent";
 
-export default function FileTypeComponent({ document }) {
-  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+export default function FileTypeComponent({ document,action }) {
   const [open, setOpen] = useState(false);
   const [dialogMessage, setDialogMessage] = useState("");
   const [DialogTitle, setDialogTitle] = useState("");
@@ -21,10 +18,10 @@ export default function FileTypeComponent({ document }) {
     setDialogMessage("test")
    setDialogTitle("test title")
   };
-
   return (
-    <div>
-      <DialogModal
+
+    <div className="flex bg-[#ECECEC] p-[18px] items-end justify-between w-[236px] cursor-pointer">
+       <DialogModal
         open={open}
         setOpen={setOpen}
         message={dialogMessage}
@@ -32,30 +29,29 @@ export default function FileTypeComponent({ document }) {
         action={deleteapiDocumet}
         buttonText={"Delete"}
       />
-      <AppModal
-        modalIsOpen={modalIsOpen}
-        setIsOpen={setModalIsOpen}
-        title="Document"
-      >
-        <DocumentsContent 
-         document={document} />
-      </AppModal>
-      <div className="flex bg-[#ECECEC] p-[18px] items-end justify-between w-[236px] cursor-pointer">
-
+      <div>
+        <img
+          src={`${document.type == "pdf" ? `${pdfIcon}` : `${docsIcon}`}`}
+          alt="icon"
+        />
+        <div className="mt-6">
+          <span className="font-bold text-[13px]">File</span> <span className="font-medium text-[12px]">{document.name}</span>{" "}
+        </div>
         <div>
-          <img
-            src={`${document.type == "pdf" ? `${pdfIcon}` : `${docsIcon}`}`}
-            alt="icon"
-          />
-          <div className="mt-6">
-            <span className="font-bold text-[13px]">File</span>{" "}
-            <span className="font-medium text-[12px]">{document.name}</span>{" "}
+          <span>Date</span> <span>{document.date}</span>{" "}
+        </div>
+      </div>
+      <div className="w-[37px]">
+        <div>
+          <input type="checkbox" />
+        </div>
+        <div className="bg-[white] rounded h-[154px] flex flex-col items-center justify-around mt-4">
+          <div>
+            <AiFillEye className="cursor-pointer text-[20px]" />{" "}
           </div>
           <div>
-            <span>Date</span> <span>{document.date}</span>{" "}
+            <AiFillEdit className="cursor-pointer text-[20px]"  />
           </div>
-        </div>
-        <div className="w-[37px]">
           <div>
             <AiFillDelete className="cursor-pointer text-[20px]" onClick={() => 
     deleteDocument()
