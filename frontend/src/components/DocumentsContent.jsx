c0a44896630af5144cae94d04b493e5e10630d38
Joynas <Ibinichinasajoy@gmail.com>
2023-08-25 12:12:44 +0100

    Feature/TIB-86-Add-Work-Stage-to-Budget

--- frontend/src/components/DocumentsContent.jsx
+++ frontend/src/components/DocumentsContent.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { imageBaseUrl } from "../services/apiservices/urls";
-import { Document, Page } from "@react-pdf/renderer";
+import { Document, Page } from "@react-pdf";
 import { Viewer } from "react-doc-viewer";
+// import { pdfjs } from 'react-pdf';
 
 export default function DocumentsContent({ document }) {
   function renderDocument(fileName) {
     const extentionType = fileName.split(".").pop().toLowerCase();
-   // fileName checks the extension of the file to determine how to render the document.
+
+    console.log(`${imageBaseUrl}/${document.file}`, "KKKKKK");
+    // fileName checks the extension of the file to determine how to render the document.
 
     const validImageExtensions = ["jpg", "jpeg", "png", "gif", "bmp"];
 
@@ -14,6 +17,9 @@ export default function DocumentsContent({ document }) {
 
     if (validImageExtensions.includes(extentionType)) {
       return <img src={`${imageBaseUrl}/${document.file}`} />;
+
+     
+
     } else if (extentionType == "pdf") {
       return (
         <Document file={`${imageBaseUrl}/${document.file}`}>
@@ -22,13 +28,15 @@ export default function DocumentsContent({ document }) {
       );
     } else if (validDocExtensions.includes(extentionType)) {
       return <Viewer file={`${imageBaseUrl}/${document.file}`} />;
-
     } else {
       return <img src={`${imageBaseUrl}/${document.file}`} />;
     }
   }
 
+
   return (
-    <div>{renderDocument(document.file)}</div>
-  )
+    <div>
+      {renderDocument(document.file)}
+    </div>
+  );
 }
--- frontend/src/pages/projects/budgetAndPlanning/BudgetAndPlanning.jsx
+++ frontend/src/pages/projects/budgetAndPlanning/BudgetAndPlanning.jsx
@@ -35,8 +35,42 @@ export default function BudgetAndPlanning() {
   return (
     <div className="bg-[white] p-[47px]">
       <BreadCrumb breadCrumbs={breadCrumbs} />
-      <hr />
-      <TabComponent components={components} />
+      <hr className="border"></hr>
+      <div className="mt-20 mb-6 text-2xl flex justify-start font-semibold">
+        View Budget
+      </div>
+      <hr className="border"></hr>
+      <div className="flex space-x-72 mt-10">
+        <div className="space-y-4">
+          <div className="text-xl font-semibold">Budget Title</div>
+          <div className="text-current">Budget 1 (Structure)</div>
+        </div>
+        <div className="space-y-4">
+          <div className="text-xl font-semibold">No of Units</div>
+          <div>20</div>
+        </div>
+        <div className="space-y-4">
+          <div className="text-xl font-semibold">Budget Cost (#)</div>
+          <div>40,317,000.00</div>
+        </div>
+      </div>
+      <div className="border h-48 w-[100%] rounded-lg mt-10">
+        <div className="mt-4 flex justify-start ml-2 text-xl font-semibold">
+          Budget Description
+        </div>
+        <div className="line-clamp-3 mt-4 flex justify-start space-y-12 line-clamp-3">
+          Lorem ipsum is a placeholder text commonly used to demonstrate the
+          visual form of a document or a typeface without relying on meaningful
+          content.
+        </div>
+      </div>
+      <div className="flex space-x-28 mt-28 mb-6 text-2xl ">
+        <div
+        >Substructure</div>
+        <div>Superstructure</div>
+      </div>
+      <hr className="border"></hr>
+      {/* <TabComponent components={components} /> */}
     </div>
   );
 }
