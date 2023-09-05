048dda404d31ff63d49bf7ca6f01be75a4acf106
Joynas <Ibinichinasajoy@gmail.com>
2023-09-05 10:46:09 +0100

    modified-pdf

--- frontend/src/components/DocumentsContent.jsx
+++ frontend/src/components/DocumentsContent.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { imageBaseUrl } from "../services/apiservices/urls";
-import { Document, Page } from "@react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 import { Viewer } from "react-doc-viewer";
-// import { pdfjs } from 'react-pdf';
+
+// import 'react-pdf/dist/Page/AnnotationLayer.css';
+// import 'react-pdf/dist/Page/TextLayer.css';
+import {useState } from "react"
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+console.log(pdfjs.version, "ok");
+
+// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 export default function DocumentsContent({ document }) {
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   function renderDocument(fileName) {
     const extentionType = fileName.split(".").pop().toLowerCase();
 
-    console.log(`${imageBaseUrl}/${document.file}`, "KKKKKK");
+    console.log(`${imageBaseUrl}/${document.file}`, "KKKKKK", extentionType);
     // fileName checks the extension of the file to determine how to render the document.
 
     const validImageExtensions = ["jpg", "jpeg", "png", "gif", "bmp"];
@@ -17,14 +36,21 @@ export default function DocumentsContent({ document }) {
 
     if (validImageExtensions.includes(extentionType)) {
       return <img src={`${imageBaseUrl}/${document.file}`} />;
-
-     
-
     } else if (extentionType == "pdf") {
+      console.log("ooooooppp");
       return (
-        <Document file={`${imageBaseUrl}/${document.file}`}>
-          <Page pageNumber={1} />
-        </Document>
+        <div>
+          {`${imageBaseUrl}/${document.file}`}
+          <Document
+            file={`${imageBaseUrl}/${document.file}`}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+        </div>
       );
     } else if (validDocExtensions.includes(extentionType)) {
       return <Viewer file={`${imageBaseUrl}/${document.file}`} />;
@@ -33,10 +59,5 @@ export default function DocumentsContent({ document }) {
     }
   }
 
-
-  return (
-    <div>
-      {renderDocument(document.file)}
-    </div>
-  );
+  return <div>{renderDocument(document.file)}</div>;
 }
