9fb44af9fe0d0438175565aa5168b3b0a319c850
Brisstone <okolijohnson69@gmail.com>
2024-01-22 04:29:05 +0100

    OJ-fixes

---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <div className='h-[100vh] flex justify-center items-center text-[40px]'>Opps!!! Page Not Found</div>
+  )
+}
